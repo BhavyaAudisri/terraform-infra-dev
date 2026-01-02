dbea5adbc24b3743e9166f25d42efdf272bb5887
bhavyaaudisri <bhavyacse40@gmail.com>
2026-01-02 13:30:16 +0530

    bucket name updated

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "somi-tf-dev"
+    bucket = "somi-tf-vpc"
     key    = "dev-bastion-test" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "somi-tf-dev"
+    dynamodb_table = "somi-tf-vpc"
   }
 }
 
