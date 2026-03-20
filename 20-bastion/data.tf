76e5e1d9141efc136acdbb273c04c93822b61679
bhavyaaudisri <bhavyacse40@gmail.com>
2026-03-20 21:39:06 +0530

    updated changes

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "joindevops" {
     most_recent      = true
-    owners           = ["250935839460"]
+    owners           = ["973714476881"]
     filter {
         name   = "name"
         values = ["RHEL-9-DevOps-Practice"]
